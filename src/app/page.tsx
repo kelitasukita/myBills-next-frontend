d7a35f414888478721b859ef8b7af0a1c6ad97ec
Kelita Lamar <kelitasukita@gmail.com>
2023-05-11 21:05:47 +0200

    Create login page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
 
-
 export default function Home() {
   return (
-    <h1>My.Bills</h1>
+    <>
+      <main className="">
+        <div>
+          <h1>my.bill$</h1>
+        </div>
+        <div>
+          <h2>Choose your login</h2>
+        </div>
+        <div>
+          <button>Login with google</button>
+        </div>
+        <div>
+          <p>or</p>
+        </div>
+        <div>
+          <label>Your email:</label>
+          <input type="email" />
+          <label>Your Password:</label>
+          <input type="password" />
+        </div>
+        <button>Login</button>
+      </main>
+    </>
   )
 }

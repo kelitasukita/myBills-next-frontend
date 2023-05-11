0f11b1c388e48a7f5312071617208a5c059305e3
Kelita Lamar <kelitasukita@gmail.com>
2023-05-11 21:58:19 +0200

    Apply styles to the login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,30 @@
 export default function Home() {
   return (
     <>
-      <main className="">
+      <main className="bg-white w-4/5 h-55vh m-auto">
         <div>
           <h1>my.bill$</h1>
         </div>
+
         <div>
           <h2>Choose your login</h2>
         </div>
+
         <div>
           <button>Login with google</button>
         </div>
+
         <div>
           <p>or</p>
         </div>
+
         <div>
           <label>Your email:</label>
           <input type="email" />
           <label>Your Password:</label>
           <input type="password" />
         </div>
+
         <button>Login</button>
       </main>
     </>

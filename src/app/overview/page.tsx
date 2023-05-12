e55bceb8638aa7ef6ed3e18a00f348346cd7cd1c
Kelita Lamar <kelitasukita@gmail.com>
2023-05-12 20:52:18 +0200

    Import addInvoice and Invoice component

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,7 +1,13 @@
+import AddInvoice from "../components/addInvoice";
+import Invoice from "../components/invoice";
 import Navbar from "../components/navbar";
 
 export default function Overview() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <AddInvoice />
+      <Invoice />
+    </>
   )
 }

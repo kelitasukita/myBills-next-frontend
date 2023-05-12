cc396a6be43e386ed446479de68cfd2f97a2ad83
Kelita Lamar <kelitasukita@gmail.com>
2023-05-12 21:11:59 +0200

    Create two news invoice status

--- src/app/components/invoice.tsx
+++ src/app/components/invoice.tsx
@@ -13,12 +13,28 @@ export default function Invoice() {
       <span className="col-start-3 col-end-3 row-start-1 text-right text-slate-400 body sm:col-start-3 sm:text-center">24/360</span>
 
       <div className="col-start-3 col-end-3 row-start-2 row-end-3 flex justify-end items-center sm:col-start-5 sm:col-end-5 sm:row-start-1 sm:mt-0 sm:justify-between">
-        <div className="flex justify-center items-center w-26 h-10 mt-12 mr-0 bg-green-400 rounded-md bg-opacity-10 sm:mt-0 sm:m-auto">
+        <div className="flex justify-center items-center w-26 h-11 mt-12 mr-0 bg-green-400 rounded-md bg-opacity-10 sm:mt-0 sm:m-auto">
           <div className="w-2 h-2 rounded-full bg-green-400"></div>
           <span className="heading-s ml-2 rounded-md text-green-400">Paid</span>
         </div>
         <Image className="hidden sm:block sm:grid-start-5" src="/icon-arrow-right.svg" alt="arrow right" width="8" height="8" />
       </div>
+
+      {/* <div className="col-start-3 col-end-3 row-start-2 row-end-3 flex justify-end items-center sm:col-start-5 sm:col-end-5 sm:row-start-1 sm:mt-0 sm:justify-between">
+        <div className="flex justify-center items-center w-26 h-11 mt-12 mr-0 bg-orange-500 rounded-md bg-opacity-20 sm:mt-0 sm:m-auto">
+          <div className="w-2 h-2 rounded-full bg-orange-400"></div>
+          <span className="heading-s ml-2 rounded-md text-orange-400">Pending</span>
+        </div>
+        <Image className="hidden sm:block sm:grid-start-5" src="/icon-arrow-right.svg" alt="arrow right" width="8" height="8" />
+      </div> */}
+
+      {/* <div className="col-start-3 col-end-3 row-start-2 row-end-3 flex justify-end items-center sm:col-start-5 sm:col-end-5 sm:row-start-1 sm:mt-0 sm:justify-between">
+        <div className="flex justify-center items-center w-26 h-11 mt-12 mr-0 bg-gray-800 rounded-md bg-opacity-20 sm:mt-0 sm:m-auto">
+          <div className="w-2 h-2 rounded-full bg-gray-800"></div>
+          <span className="heading-s ml-2 rounded-md text-gray-800">Draft</span>
+        </div>
+        <Image className="hidden sm:block sm:grid-start-5" src="/icon-arrow-right.svg" alt="arrow right" width="8" height="8" />
+      </div> */}
     </article>
   )
 }

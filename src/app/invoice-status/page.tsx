4a2ff8275e72cfe727ca887e8394d3cef1f89dc1
Kelita Lamar <kelitasukita@gmail.com>
2023-05-13 00:21:13 +0200

    Create a new page invoice status(list)

---
+++ src/app/invoice-status/page.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import Navbar from "../components/navbar";
+
+export default function InvoiceStatus() {
+  return (
+    <>
+      <Navbar />
+      <button className="flex justify-center items-center float-left w-1/3 mx-auto mt-3 py-5 mb-3">
+        <Image src="/icon-arrow-left.svg" alt="left arrow" width="7" height="10" />
+        <p className="heading-s ml-7">Go back</p>
+      </button>
+
+      <article className="flex justify-between items-center w-11/12 mx-auto p-6 rounded-lg bg-white">
+        <span className="body-variant text-gray-400">Status</span>
+        <div className="flex justify-center items-center w-26 h-11 mr-0 bg-orange-500 rounded-md bg-opacity-10 sm:mt-0 sm:m-auto">
+          <div className="w-2 h-2 rounded-full bg-orange-400"></div>
+          <span className="heading-s ml-2 rounded-md text-orange-400">Pending</span>
+        </div>
+      </article>
+
+      <main className=" bg-white w-11/12 mx-auto mt-4 p-6 rounded-lg">
+        <section className="grid grid-cols-2 grid-rows-3 ">
+          <h2 className="heading-s text-gray-800 mt-8">
+            <strong className="text-gray-400">#</strong>
+            Mortgage
+          </h2>
+          <div>
+            <p className="body-variant text-gray-400 mt-8 mb-4 ">Due Date</p>
+            <span className="heading-s text-gray-800 ">01-10-2023</span>
+          </div>
+          <div>
+            <p className="body-variant text-gray-400 mt-8 mb-4 ">Type of payment</p>
+            <span className="heading-s text-gray-800 ">Automatic Debit</span>
+          </div>
+          <div>
+            <p className="body-variant text-gray-400 mt-8 mb-4 ">Recurrent</p>
+            <span className="heading-s text-gray-800 ">Yes</span>
+          </div>
+          <div>
+            <p className="body-variant text-gray-400 mt-8 mb-4 ">Installments</p>
+            <span className="heading-s text-gray-800 ">24/360</span>
+          </div>
+          <div>
+            <p className="body-variant text-gray-400 mt-8 mb-4 ">Observation</p>
+            <span className="heading-s text-gray-800 ">This is an optional information</span>
+          </div>
+        </section>
+        <div className="flex justify-around items-center mt-8 mb-4 h-20 rounded-lg  bg-gray-700">
+          <span className="body-variant text-white">Value</span>
+          <strong className="heading-l text-white">€ 1800.00</strong>
+        </div>
+      </main>
+
+      <footer className="flex justify-between items-center w-11/12 mx-auto mt-4 p-6 rounded-lg bg-white">
+        <button>Edit</button>
+        <button>Delete</button>
+        <button>Mark as Paid</button>
+      </footer>
+    </>
+  )
+}

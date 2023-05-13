6d3f10e404025b202a79ba461827830154825fa2
Kelita Lamar <kelitasukita@gmail.com>
2023-05-13 23:43:37 +0200

    Apply transparent bg to the date field

--- src/app/edit-invoice/page.tsx
+++ src/app/edit-invoice/page.tsx
@@ -18,39 +18,39 @@ export default function EditInvoice() {
 
           <form action="/" className="flex flex-col mt-12">
             <label className="body-variant text-gray-400 mb-4  ">Description</label>
-            <input className="border border-gray-300 h-12 rounded-md p-4 mb-6" type="text" />
+            <input className="heading-s text-gray-800 border border-gray-300 h-12 rounded-md p-4 mb-6" type="text" />
 
             <label className="body-variant text-gray-400 mb-4  ">Currency</label>
             <div className="flex justify-start items-center mb-8">
-              <input className="appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 " type="radio" name="currency" />
+              <input className="heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 " type="radio" name="currency" />
               <label>EUR</label>
-              <input className="appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 ml-4" type="radio" name="currency" />
+              <input className="heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 ml-4" type="radio" name="currency" />
               <label>BRL</label>
-              <input className="appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 ml-4" type="radio" name="currency" />
+              <input className="heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 ml-4" type="radio" name="currency" />
               <label>USD</label>
             </div>
 
             <label className="body-variant text-gray-400 mb-4  ">Value</label>
-            <input className="border border-gray-300 h-12 rounded-md p-4 mb-6" type="number" />
+            <input className="heading-s text-gray-800 border border-gray-300 h-12 rounded-md p-4 mb-6" type="number" />
 
             <label className="body-variant text-gray-400 mb-4  ">Due date</label>
-            <input className="border border-gray-300 h-12 rounded-md p-4 mb-6" type="date" />
+            <input className="heading-s text-gray-800 bg-transparent border border-gray-300 h-12 rounded-md p-4 mb-6" type="date" />
 
             <label className="body-variant text-gray-400 mb-4  ">Automatic Debit</label>
-            <input className="appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-6 w-6 mb-6" type="checkbox" />
+            <input className="heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-6 w-6 mb-6" type="checkbox" />
 
             <label className="body-variant text-gray-400 mb-4  ">Recurrent</label>
-            <input className="appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-6 w-6 mb-6" type="checkbox" />
+            <input className="heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-6 w-6 mb-6" type="checkbox" />
 
             <label className="body-variant text-gray-400 mb-4  ">Installments</label>
             <div className="flex justify-start items-center">
-              <input className="border border-gray-300 h-12 w-20 rounded-md p-4 mb-6" type="number" />
+              <input className="heading-s text-gray-800 border border-gray-300 h-12 w-20 rounded-md p-4 mb-6" type="number" />
               <span className="body-variant text-gray-400 text-center mx-2 h-10">of</span>
-              <input className="border border-gray-300 h-12 w-20 rounded-md p-4 mb-6" type="number" />
+              <input className="heading-s text-gray-800 border border-gray-300 h-12 w-20 rounded-md p-4 mb-6" type="number" />
             </div>
 
             <label className="body-variant text-gray-400 mb-4  ">Observation</label>
-            <input className="border border-gray-300 h-12 rounded-md p-4 mb-6" type="text" />
+            <input className="heading-s text-gray-800 border border-gray-300 h-12 rounded-md p-4 mb-6" type="text" />
           </form>
         </article>
 

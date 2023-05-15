b03ae385eb1c4c7a877cf6574b6ac44048bc4a50
Kelita Lamar <kelitasukita@gmail.com>
2023-05-15 12:13:39 +0200

    Set margin left to the component

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,5 +1,5 @@
 export default function Button(props: any | undefined) {
   return (
-    <button className={`heading-s ${props.textColor} py-5 px-6 ${props.bgColor} rounded-full hover:opacity-80`}>{props.name}</button>
+    <button className={`heading-s ${props.textColor} py-5 px-6 ${props.bgColor} rounded-full hover:opacity-80 ml-2`}>{props.name}</button>
   )
 }

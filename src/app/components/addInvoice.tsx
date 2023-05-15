11be8208b154ba4a06712eef8dd511a4f236f8cf
Kelita Lamar <kelitasukita@gmail.com>
2023-05-15 12:13:00 +0200

    Delete responsive old code

--- src/app/components/addInvoice.tsx
+++ src/app/components/addInvoice.tsx
@@ -25,10 +25,10 @@ export default function AddInvoice() {
   }, []);
 
   return (
-    <article className="flex justify-between items-center w-11/12 mx-auto mt-6 desktop:w-6-12 desktop:mx-auto desktop:w-730">
+    <article className="flex justify-between items-center w-11/12 mx-auto mt-6 lg:mt-20">
       <div>
-        <h2 className="text-gray-800 heading-m mb-1 sm:text-4xl sm:leading-8 sm:-tracking-1 sm:font-bold " >Invoices</h2>
-        <div className="text-gray-400 body-variant mt-1 sm:text-sm sm:eading-4.5 sm:-tracking-0.1 sm:font-medium">
+        <h2 className="text-gray-800 heading-m mb-1">Invoices</h2>
+        <div className="text-gray-400 body-variant mt-3">
           {width > 640
             ?
             (<span>There are 7 total invoices</span>)
@@ -52,12 +52,12 @@ export default function AddInvoice() {
         </div>
         <div>
           <div>
-            <div className="flex justify-between items-center w-90 bg-violet-500 rounded-full p-1.5 sm:w-150 sm:h-48">
+            <div className="flex justify-between items-center w-90 bg-violet-500 rounded-full p-1.5">
               <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full " >
                 <Image className="" src="/icon-plus.svg" alt="plus signal" width="12" height="12" />
               </div>
               <div>
-                <p className="text-sm text-white font-bold ml-1 sm:mt-1">
+                <p className="text-sm text-white font-bold ml-1 ">
                   {width > 639
                     ?
                     (<span>New Invoice</span>)

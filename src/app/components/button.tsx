11c1748f644beabd50e9a96bc831383bf265dae7
Kelita Lamar <kelitasukita@gmail.com>
2023-05-15 21:52:19 +0200

    Fix button height and width

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,5 +1,5 @@
 export default function Button(props: any | undefined) {
   return (
-    <button className={`heading-s ${props.textColor} py-5 px-6 ${props.bgColor} rounded-full hover:opacity-80 ml-2`}>{props.name}</button>
+    <button className={`heading-s ${props.textColor} h-11 whitespace-nowrap px-4 ${props.bgColor} rounded-full hover:opacity-80 ml-2`}>{props.name}</button>
   )
 }

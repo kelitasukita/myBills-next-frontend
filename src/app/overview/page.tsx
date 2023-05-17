a2d68516e8e712623acfdaf06bb419feedb0c00f
Kelita Lamar <kelitasukita@gmail.com>
2023-05-17 21:07:53 +0200

    Call Edit Screen modal to test

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,17 +1,19 @@
 import AddInvoice from "../components/addInvoice";
+import EditScreenModal from "../components/EditScreenModal";
 import EmptyInvoice from "../components/emptyInvoice";
 import Invoice from "../components/invoice";
 import Navbar from "../components/navbar";
 
 export default function Overview() {
   return (
-    <div className="lg:flex">
-      <Navbar />
-      <div className="lg:w-736 mx-auto">
-        <AddInvoice />
-        <Invoice />
-        {/* <EmptyInvoice /> */}
-      </div>
-    </div>
+    <EditScreenModal />
+    // <div className="lg:flex">
+    //   {/* <Navbar />
+    //   <div className="lg:w-736 mx-auto">
+    //     <AddInvoice />
+    //     <Invoice />
+    //     {/* <EmptyInvoice /> */}
+    //   </div> */}
+    // </div>
   )
 }

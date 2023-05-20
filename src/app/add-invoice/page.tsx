ec4582a70500bf1e27a71767febe5e69938cad81
Kelita Lamar <kelitasukita@gmail.com>
2023-05-20 21:45:41 +0200

    Set responsive design to both, edit & add invoice

--- src/app/add-invoice/page.tsx
+++ src/app/add-invoice/page.tsx
@@ -1,70 +1,103 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+import AddInvoiceModal from "../components/AddInvoiceModal";
+import Button from "../components/button";
 import GoBackBtn from "../components/goBackBtn";
 import Navbar from "../components/navbar";
 
 export default function AddInvoice() {
+
+  function getSize() {
+    if (typeof window !== "undefined") {
+      return window.innerWidth;
+    }
+    return 0;
+  }
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(getSize());
+      console.log(getSize());
+    }
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    // return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <main>
-      <Navbar />
-      <div className="flex flex-col bg-white">
-        <div className="pl-6">
-          <GoBackBtn />
-        </div>
-        <article className="p-6">
-          <header className="flex justify-start items-center">
-            <h1 className="heading-m text-gray-800">New Invoice</h1>
-          </header>
-
-          <form action="/" className="flex flex-col mt-12">
-            <label className="body-variant text-gray-400 mb-4  ">Description</label>
-            <input className="focus:border-violet-500 heading-s text-gray-800 border border-gray-300 h-12 rounded-md p-4 mb-6" type="text" />
-
-            <label className="body-variant text-gray-400 mb-4  ">Currency</label>
-            <div className="flex justify-start items-center mb-8">
-              <input className="focus:border-violet-500 heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 " type="radio" name="currency" />
-              <label>EUR</label>
-              <input className="focus:border-violet-500 heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 ml-4" type="radio" name="currency" />
-              <label>BRL</label>
-              <input className="focus:border-violet-500 heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 ml-4" type="radio" name="currency" />
-              <label>USD</label>
+    <>
+      {width < 640
+        ?
+        (<main>
+          <Navbar />
+          <div className="flex flex-col bg-white">
+            <div className="pl-6">
+              <GoBackBtn />
             </div>
+            <article className="p-6">
+              <header className="flex justify-start items-center">
+                <h1 className="heading-m text-gray-800">New Invoice</h1>
+              </header>
 
-            <label className="body-variant text-gray-400 mb-4  ">Value</label>
-            <input className="focus:border-violet-500 heading-s text-gray-800 border border-gray-300 h-12 rounded-md p-4 mb-6" type="number" />
+              <form action="/" className="flex flex-col mt-12">
+                <label className="body-variant text-gray-400 mb-4  ">Description</label>
+                <input className="focus:border-violet-500 heading-s text-gray-800 border border-gray-300 h-12 rounded-md p-4 mb-6" type="text" />
 
-            <label className="body-variant text-gray-400 mb-4  ">Due date</label>
-            <input className="focus:border-violet-500 heading-s text-gray-800 bg-transparent border border-gray-300 h-12 rounded-md p-4 mb-6" type="date" />
+                <label className="body-variant text-gray-400 mb-4  ">Currency</label>
+                <div className="flex justify-start items-center mb-8">
+                  <input className="focus:border-violet-500 heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 " type="radio" name="currency" />
+                  <label>EUR</label>
+                  <input className="focus:border-violet-500 heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 ml-4" type="radio" name="currency" />
+                  <label>BRL</label>
+                  <input className="focus:border-violet-500 heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-5 w-5 mr-1 ml-4" type="radio" name="currency" />
+                  <label>USD</label>
+                </div>
 
-            <div className="grid grid-cols-2">
-              <div className="flex flex-col justify-start">
-                <label className="body-variant text-gray-400 mb-4">Automatic Debit</label>
-                <input className="focus:border-violet-500 heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-6 w-6 mb-6" type="checkbox" />
-              </div>
-              <div className="flex flex-col">
-                <label className="body-variant text-gray-400 mb-4  ">Recurrent</label>
-                <input className="focus:border-violet-500 heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-6 w-6 mb-6" type="checkbox" />
-              </div>
-            </div>
+                <label className="body-variant text-gray-400 mb-4  ">Value</label>
+                <input className="focus:border-violet-500 heading-s text-gray-800 border border-gray-300 h-12 rounded-md p-4 mb-6" type="number" />
 
-            <label className="body-variant text-gray-400 mb-4  ">Installments</label>
-            <div className="flex justify-start items-center">
-              <input className="focus:border-violet-500 heading-s text-gray-800 border border-gray-300 h-12 w-20 rounded-md p-4 mb-6" type="number" />
-              <span className="body-variant text-gray-400 text-center mx-2 h-10">of</span>
-              <input className="focus:border-violet-500 heading-s text-gray-800 border border-gray-300 h-12 w-20 rounded-md p-4 mb-6" type="number" />
-            </div>
+                <label className="body-variant text-gray-400 mb-4  ">Due date</label>
+                <input className="focus:border-violet-500 heading-s text-gray-800 bg-transparent border border-gray-300 h-12 rounded-md p-4 mb-6" type="date" />
 
-            <label className="body-variant text-gray-400 mb-4  ">Observation</label>
-            <input className="focus:border-violet-500 heading-s text-gray-800 border border-gray-300 h-12 rounded-md p-4 mb-6" type="text" />
-          </form>
-        </article>
+                <div className="grid grid-cols-2">
+                  <div className="flex flex-col justify-start">
+                    <label className="body-variant text-gray-400 mb-4">Automatic Debit</label>
+                    <input className="focus:border-violet-500 heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-6 w-6 mb-6" type="checkbox" />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="body-variant text-gray-400 mb-4  ">Recurrent</label>
+                    <input className="focus:border-violet-500 heading-s text-gray-800 appearance-none rounded-full checked:bg-violet-600 border border-gray-300 h-6 w-6 mb-6" type="checkbox" />
+                  </div>
+                </div>
 
-        <div className="h-16 bg-line bg-gradient-to-b from-transparent to-gray-200" />
+                <label className="body-variant text-gray-400 mb-4  ">Installments</label>
+                <div className="flex justify-start items-center">
+                  <input className="focus:border-violet-500 heading-s text-gray-800 border border-gray-300 h-12 w-20 rounded-md p-4 mb-6" type="number" />
+                  <span className="body-variant text-gray-400 text-center mx-2 h-10">of</span>
+                  <input className="focus:border-violet-500 heading-s text-gray-800 border border-gray-300 h-12 w-20 rounded-md p-4 mb-6" type="number" />
+                </div>
 
-        <footer className="flex justify-between items-center w-full p-6 bg-white">
-          <button className="text-sm font-bold text-gray-400 py-5 px-6 bg-gray-100 rounded-full">Discard</button>
-          <button className="text-sm font-bold text-gray-400 py-5 px-4 bg-gray-700 rounded-full">Save as Draft</button>
-          <button className="text-sm font-bold text-gray-100 py-5 px-4 bg-violet-500 rounded-full">Save & Send</button>
-        </footer>
-      </div>
-    </main>
+                <label className="body-variant text-gray-400 mb-4  ">Observation</label>
+                <input className="focus:border-violet-500 heading-s text-gray-800 border border-gray-300 h-12 rounded-md p-4 mb-6" type="text" />
+              </form>
+            </article>
+
+            <div className="h-16 bg-line bg-gradient-to-b from-transparent to-gray-200" />
+
+            <footer className="flex justify-between items-center w-full p-6 bg-white">
+              <Button textColor="text-gray-400" bgColor="bg-gray-100" name="Discard" />
+              <Button textColor="text-gray-400" bgColor="bg-gray-700" name="Save as Draft" />
+              <Button textColor="text-gray-100" bgColor="bg-violet-500" name="Save & Send" />
+            </footer>
+          </div>
+        </main>)
+        :
+        <AddInvoiceModal />
+      }
+    </>
   )
 }
--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,13 +1,9 @@
 import OverviewPage from "../components/OverviewPage";
-import EditScreenModal from "../components/EditScreenModal";
-import EmptyInvoice from "../components/emptyInvoice";
 import Invoice from "../components/invoice";
 import Navbar from "../components/navbar";
 
-
 export default function Overview() {
   return (
-    // <EditScreenModal />
     <div className="lg:flex">
       <Navbar />
       <div className="lg:w-736 mx-auto">

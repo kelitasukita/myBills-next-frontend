558f227a5bb20474a58e9d9971ca932e564d7cfd
Kelita Lamar <kelitasukita@gmail.com>
2023-05-24 23:31:51 +0200

    [fix] import typo

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,7 +1,7 @@
 import HeaderOverview from "../components/HeaderOverview";
 import InvoiceItem from "../components/InvoiceItem";
 import Navbar from "../components/navbar";
-import EmptyInvoice from "../components/emptyInvoice";
+import EmptyInvoice from "../components/EmptyInvoice";
 
 export default function Overview() {
   return (

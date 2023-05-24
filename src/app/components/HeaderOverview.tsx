e5c753231d2a69cb339e1f64676cebc1ec4d8657
Kelita Lamar <kelitasukita@gmail.com>
2023-05-24 23:33:14 +0200

    [WIP] start the modal implementation
    Just implemented the open modal, still have to do the close

--- src/app/components/HeaderOverview.tsx
+++ src/app/components/HeaderOverview.tsx
@@ -14,6 +14,7 @@ function getSize() {
 
 export default function HeaderOverview() {
   const [width, setWidth] = useState(0);
+  const [isInvoiceModalOpen, setInvoiceModalOpen] = useState(false);
 
   useEffect(() => {
     function handleResize() {
@@ -26,37 +27,43 @@ export default function HeaderOverview() {
     // return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  const handleModal = () => {
+    setInvoiceModalOpen(true);
+  }
+
   return (
-    <article className="flex justify-between items-center w-11/12 mx-auto mt-8 sm:mt-16 lg:mt-20 lg:w-full">
-      <div>
-        <h2 className="text-gray-800 heading-m mb-1">Invoices</h2>
-        <div className="text-gray-400 body-variant mt-3">
-          {width > 640
-            ?
-            (<span>There are 7 total invoices</span>)
-            :
-            (<span> 7 invoices</span>)
-          }
-        </div>
-      </div>
-
-      <div className="flex justify-center items-center">
-        <div className="relative cursor-pointer">
-
-          <div className="flex justify-between items-center mr-6 md:mr-10">
-            <p className="text-base font-bold text-gray-800 leading-4 mr-4 text-left">
-              {width > 640
-                ?
-                (<span>Filter by status</span>)
-                :
-                (<span>Filter</span>)
-              }
-            </p>
-            <Image src="/icon-arrow-down.svg" alt="down arrow" width="12" height="12" />
+    <>
+      {isInvoiceModalOpen && <AddInvoiceModal />}
+      <article className="flex justify-between items-center w-11/12 mx-auto mt-8 sm:mt-16 lg:mt-20 lg:w-full">
+        <div>
+          <h2 className="text-gray-800 heading-m mb-1">Invoices</h2>
+          <div className="text-gray-400 body-variant mt-3">
+            {width > 640
+              ?
+              (<span>There are 7 total invoices</span>)
+              :
+              (<span> 7 invoices</span>)
+            }
           </div>
+        </div>
 
-          {/*------------------------------------- DROPDOWN MENU -------------------------------- */}
-          {/* <div className="">
+        <div className="flex justify-center items-center">
+          <div className="relative cursor-pointer">
+
+            <div className="flex justify-between items-center mr-6 md:mr-10">
+              <p className="text-base font-bold text-gray-800 leading-4 mr-4 text-left">
+                {width > 640
+                  ?
+                  (<span>Filter by status</span>)
+                  :
+                  (<span>Filter</span>)
+                }
+              </p>
+              <Image src="/icon-arrow-down.svg" alt="down arrow" width="12" height="12" />
+            </div>
+
+            {/*------------------------------------- DROPDOWN MENU -------------------------------- */}
+            {/* <div className="">
             <div className="absolute top-6 md:top-8 right-5 md:right-10 flex flex-col py-4 pl-4 justify-around w-40 h-28 bg-white shadow-md rounded-lg shrink-0">
               <label className="flex justify-start items-center heading-s text-gray-800 cursor-pointer">
                 < input type="checkbox" className="cursor-pointer mr-3 relative appearance-none w-4 h-4 hover:border hover:border-violet-600 bg-violet-100 rounded-sm focus:outline-none checked:bg-violet-500 after:content-[''] after:w-full after:h-full after:absolute after:left-0 after:top-0 after:bg-center after:bg-no-repeat after:bg-[length:10px] checked:after:bg-[url('../../public/checked.svg')]" />
@@ -72,30 +79,30 @@ export default function HeaderOverview() {
               </label>
             </div>
           </div> */}
-        </div>
+          </div>
 
-        <div>
           <div>
-            <div className="cursor-pointer flex justify-end items-center w-90 bg-violet-500 hover:bg-violet-400 hover:transition hover:duration-400 rounded-full p-1.5">
-              <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full " >
-                <Image className="" src="/icon-plus.svg" alt="plus signal" width="12" height="12" />
-              </div>
-              <div>
-                <p className="text-xs text-white font-bold pl-3 pr-2 ">
-                  {width > 639
-                    ?
-                    (<span>New Invoice</span>)
-                    :
-                    (<span>New</span>)
-                  }
-                </p>
+            <div>
+              <div className="cursor-pointer flex justify-end items-center w-90 bg-violet-500 hover:bg-violet-400 hover:transition hover:duration-400 rounded-full p-1.5" onClick={handleModal}>
+                <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full " >
+                  <Image className="" src="/icon-plus.svg" alt="plus signal" width="12" height="12" />
+                </div>
+                <div>
+                  <p className="text-xs text-white font-bold pl-3 pr-2 ">
+                    {width > 639
+                      ?
+                      (<span>New Invoice</span>)
+                      :
+                      (<span>New</span>)
+                    }
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-    </article>
+      </article>
+    </>
   )
 
 }

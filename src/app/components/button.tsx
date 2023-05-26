a576bbfd1b6b2fec3203235633a2c42fb05d86ad
Kelita Lamar <kelitasukita@gmail.com>
2023-05-26 10:06:44 +0200

    Add onClick property on button component

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,5 +1,6 @@
 export default function Button(props: any | undefined) {
   return (
-    <button className={`heading-s ${props.textColor} h-11 whitespace-nowrap px-4 ${props.bgColor} rounded-full hover:opacity-80 ml-2`}>{props.name}</button>
+    <button
+      className={`heading-s ${props.textColor} h-11 whitespace-nowrap px-4 ${props.bgColor} rounded-full hover:opacity-80 ml-2`} onClick={props.onClick}>{props.name}</button>
   )
 }

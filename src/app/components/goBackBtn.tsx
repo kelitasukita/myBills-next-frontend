a3ae35417ca87f5266ad5a9b9b667c8f7fb5d979
Kelita Lamar <kelitasukita@gmail.com>
2023-05-26 13:27:31 +0200

    Fix margin

--- src/app/components/goBackBtn.tsx
+++ src/app/components/goBackBtn.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function GoBackBtn() {
   return (
-    <button className="cursor-pointer flex justify-start items-center float-left w-1/3 mt-3 py-5 mb-3 hover:">
+    <button className="cursor-pointer flex justify-start items-center float-left w-1/3 mt-3 py-5 hover:">
       <Image src="/icon-arrow-left.svg" alt="left arrow" width="7" height="10" />
       <p className="heading-s text-gray-800 hover:text-gray-400 ml-7">Go back</p>
     </button>

c5ea431dc9d688f917b4f939992bd117db0b2add
Kelita Lamar <kelitasukita@gmail.com>
2023-06-01 11:20:30 +0200

    Create fetch function and map expenses

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,13 +1,28 @@
 import HeaderOverview from "../components/HeaderOverview";
-import InvoiceItem from "../components/InvoiceItem";
+import InvoiceItem, { InvoiceProps } from "../components/InvoiceItem";
 import EmptyInvoice from "../components/EmptyInvoice";
 
-export default function Overview() {
+async function getExpenses() {
+  const data = await fetch('http://192.168.178.243:3333/expenses', { cache: 'no-store' });
+
+  if (!data.ok) {
+    throw new Error('Failed to fetch data!');
+  }
+
+  return data.json();
+}
+
+export default async function Overview() {
+  const expenses = await getExpenses();
+
   return (
     <div className="lg:flex relative">
       <div className="lg:w-736 mx-auto">
         <HeaderOverview />
-        <InvoiceItem />
+        {expenses.map((expense: InvoiceProps) => (
+          <InvoiceItem key={expense.id} invoice={expense} />
+        ))}
+
         {/* <EmptyInvoice /> */}
       </div>
     </div>

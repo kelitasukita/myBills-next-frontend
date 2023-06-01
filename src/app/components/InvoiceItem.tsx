0835625a6cdf09809afd61921dff7506dc25a70f
Kelita Lamar <kelitasukita@gmail.com>
2023-06-01 11:49:22 +0200

    Fix date type return

--- src/app/components/InvoiceItem.tsx
+++ src/app/components/InvoiceItem.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export interface InvoiceProps {
   id: string;
   description: string;
-  dueDate: Date;
+  dueDate: string;
   value: number;
   currentInstallment: number;
   installments: number;
@@ -12,13 +12,19 @@ export interface InvoiceProps {
 }
 
 export default function InvoiceItem({ invoice }: { invoice: InvoiceProps }) {
+  const formatDate = (date: Date) => {
+
+    const month = new Date(date.toDateString()).toLocaleDateString('en-us', { month: 'short' });
+
+    return date.getDate() + ' ' + month + ' ' + date.getFullYear();
+  }
   return (
     <article className="cursor-pointer grid grid-cols-3 grid-rows-3 mt-8 w-11/12 mx-auto h-32 p-6 bg-white rounded-lg shadow-md hover:border hover:border-violet-600 hover:border-solid md:grid md:grid-cols-5 md:grid-rows-1 md:h-18 md:justify-center md:items-center md:mt-16 lg:w-full">
       <h2 className="heading-s text-gray-800 overflow-hidden truncate lg:h-5">
         <span className="text-gray-400 font-medium">#</span>
         {invoice.description}
       </h2>
-      <span className="col-start-1 col-end-2 text-slate-400 body inline-block mt-4 md:col-start-3 md:col-end-4 md:mt-0">Due {invoice.dueDate}</span>
+      <span className="col-start-1 col-end-2 text-slate-400 body inline-block mt-4 md:col-start-3 md:col-end-4 md:mt-0">Due {formatDate(new Date(invoice.dueDate))}</span>
       <strong className="col-start-1 col-end-2 text-gray-800 heading-s mt-4 md:col-start-4 md:col-end-4 md:mt-0 md:text-center">{invoice.paid || invoice.currency === 'EUR' ? '€' : 'R$'} {invoice.value}</strong>
 
       <span className="col-start-3 col-end-3 row-start-1 text-right text-slate-400 body md:col-start-2 md:text-center">{invoice.currentInstallment}/{invoice.installments}</span>
